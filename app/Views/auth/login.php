932e871945a4e7967c053eb70215628ebfe658bb
Agus Puryanto <aguspuryanto@gmail.com>
2024-08-15 09:43:20 +0700

    update layout

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -1,7 +1,7 @@
 <?= $this->extend('layouts/page_layout_login') ?>
 
 <?= $this->section('content') ?>
-<div class="row h-100 justify-content-center align-items-center">
+<!-- <div class="row h-100 justify-content-center align-items-center">
 <div class="col-md-4 offset-md-1">
     <h2>Login</h2>
     <?php if (session()->getFlashdata('msg')): ?>
@@ -19,5 +19,44 @@
         <button type="submit" class="btn btn-primary">Login</button>
     </form>
 </div>
-</div>
+</div> -->
+
+<main>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-5">
+                <div class="card shadow-lg border-0 rounded-lg mt-5">
+                    <div class="card-header"><h3 class="text-center font-weight-light my-4">Login</h3></div>
+                    <div class="card-body">
+
+                        <?php if (session()->getFlashdata('msg')): ?>
+                            <div class="alert alert-danger"><?= session()->getFlashdata('msg') ?></div>
+                        <?php endif; ?>
+                        <form>
+                            <div class="form-floating mb-3">
+                                <input class="form-control" name="inputEmail" id="inputEmail" type="email" placeholder="name@example.com" />
+                                <label for="inputEmail">Email address</label>
+                            </div>
+                            <div class="form-floating mb-3">
+                                <input class="form-control" name="inputPassword" id="inputPassword" type="password" placeholder="Password" />
+                                <label for="inputPassword">Password</label>
+                            </div>
+                            <div class="form-check mb-3">
+                                <input class="form-check-input" id="inputRememberPassword" type="checkbox" value="" />
+                                <label class="form-check-label" for="inputRememberPassword">Remember Password</label>
+                            </div>
+                            <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
+                                <a class="small" href="password.html">Forgot Password?</a>
+                                <button type="submit" class="btn btn-primary">Login</button>
+                            </div>
+                        </form>
+                    </div>
+                    <!-- <div class="card-footer text-center py-3">
+                        <div class="small"><a href="register.html">Need an account? Sign up!</a></div>
+                    </div> -->
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
 <?= $this->endSection() ?>
